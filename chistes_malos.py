99faa0d609a60da63ef8c3320569fb0c9d86484d
Sergio Navaro <sergio@orvium.io>
2020-09-16 10:59:42 +0200

    first commit

---
+++ chistes_malos.py
@@ -0,0 +1,4 @@
+CHISTES_MALOS = ['¿Por qué las focas del circo miran siempre hacia arriba? Porque es donde están los focos',
+'— ¡Estás obsesionado con la comida! No sé a que te refieres croquetamente.',
+'¿Por qué estás hablando con esas zapatillas?Porque pone "converse"',
+'¿Sabes cómo se queda un mago después de comer? Magordito']
